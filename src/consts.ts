82a2db518e0a8cf49d4a0eb1cc906b9a773b400e
dan-ukr <daniil.swed@gmail.com>
2026-03-29 19:25:30 +0200

    done

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "WeatherDude | Dress for the Weather";
+export const SITE_DESCRIPTION = "The only weather app that understands your personal vibe. Built for the dudes who care.";
